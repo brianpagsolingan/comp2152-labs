acca4610ae67ffae9b483cf9decddc06358e8782
Brian Pagsolingan <brian.pagsolingan@gmail.com>
2025-01-11 09:14:34 -0800

    lab1 coding questions

---
+++ lab1.py
@@ -0,0 +1,20 @@
+#Sample Coding Questions 01 Week 01
+#Brian Aldrin Pagsolingan 101490258
+
+arrayVar = [1,4,7,9]
+
+a = 1
+b = 2
+c = 3
+d = 4
+
+e = ((a - ((b**c) // d)) + (a % c))
+print(e)
+
+temperature = 32.6
+print(format(temperature, '.3f'))
+
+userAge = input("please enter your age: ")
+userAge += 22
+
+print(f"Now showing the shop items filtered by age: {userAge}")
