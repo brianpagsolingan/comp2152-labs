dbc280578e298f7293ed2600491e32a480ee23f7
Brian Pagsolingan <brian.pagsolingan@gmail.com>
2025-01-17 14:26:50 -0500

    lab 2. Periodic table guessing game

--- week2/profVersion.py
+++ week2/profVersion.py
@@ -10,7 +10,10 @@ for attempt in range(attempts):
     guess = input(f"Attempt at {attempt+1}/{attempts}: Guess the element: ").strip()
     if guess.lower() == randomElement.lower():
         print(f"Correct the answer was {randomElement}.")
+        break
     else:
         if attempt < attempts -1:
             print(f"Wrong guess. Hint the element starts with {randomElement[0]}."
                   f" Try again.")
+        else:
+            print(f"Wrong guess! The element was {randomElement}.")
